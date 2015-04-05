85c108e434fb2afc6d582e269c10c1461074250e
Wush Wu <wush978@gmail.com>
2015-04-05 18:44:09 +0800

    decode utf-16 for tag_list

--- R/DSPCkan.R
+++ R/DSPCkan.R
@@ -7,6 +7,26 @@ package_list <- function(as = "table") {
 
 #'@export
 tag_list <- function(as = "table") {
-  ckanr::tag_list(as = as, url = .url)
+  retval <- ckanr::tag_list(as = as, url = .url)
+  for(i in seq_len(ncol(retval))) {
+    if (class(retval[[i]]) == "character") retval[[i]] <- decode_hex_utf16(retval[[i]])
+  }
+  retval
+}
+
+#'@importFrom magrittr %>%
+decode_hex_utf16 <- function(x) {
+  m <- regmatches(x, regexec("(<U\\+[0-9a-f]{4}>)+", x))
+  retval <- x
+  for(i in seq_along(m)) {
+    if (length(m[[i]]) == 0) next
+    value <- sapply(seq_len(nchar(m[[i]][1]) %/% 8), function(j) {
+      substring(m[[i]][1], 8 * (j - 1) + 4, 8 * j - 1) %>%
+        strtoi(base = 16) %>%
+        intToUtf8
+    }) %>% paste(collapse = "")
+    retval[i] <- gsub(pattern = m[[i]][1], replacement = value, x = x[i], fixed = TRUE)
+  }
+  retval
 }
 
