6533b7912633842af285711878af277f4ef70b51
Wush Wu <wush978@gmail.com>
2015-04-06 14:17:20 +0800

    implement download_course_materials

--- R/DSPCkan.R
+++ R/DSPCkan.R
@@ -1,30 +1,72 @@
 .url <- "https://data.dsp.im"
 
+#'@importFrom magrittr %>%
 #'@importFrom httr add_headers
-#'@export
 package_list <- function(offset = 0, limit = 31, as = "table") {
   ckanr::package_list(offset = offset, limit = limit, as = as, url = .url, 
                       add_headers("X-CKAN-API-Key" = getOption("X-CKAN-API-Key", ""))) %>%
     decode_hex_utf16
 }
 
-#'@export
 tag_list <- function(as = "table") {
   ckanr::tag_list(query = NULL, vocabulary_id = NULL, all_fields = TRUE,
                   as = as, url = .url, add_headers("X-CKAN-API-Key" = getOption("X-CKAN-API-Key", ""))) %>%
     decode_hex_utf16
 }
 
-#'@export
-organization_list <- function(id, as = "table") {
+organization_list <- function(as = "table") {
   ckanr::organization_list(order_by = "name", decreasing = TRUE, organizations = NULL, all_fields = TRUE,
                           as = as, url = .url, add_headers("X-CKAN-API-Key" = getOption("X-CKAN-API-Key", ""))) %>%
     decode_hex_utf16
 }
 
+organization_show <- function(id, as = "table") {
+  ckanr::organization_show(id = id, include_datasets = TRUE, url = .url, as = as, 
+                           add_headers("X-CKAN-API-Key" = getOption("X-CKAN-API-Key", ""))) %>%
+    `[`(c("display_name", "description", "title", "package_count", "packages", "name")) %>%
+    decode_hex_utf16
+}
+
+#'@export
+download_course_materials <- function(course_name = NULL, enter_api_key = FALSE, enter_destdir = FALSE) {
+  if (is.null(getOption("X-CKAN-API-Key")) || enter_api_key) input_apikey()
+  if (is.null(course_name)) {
+    retval <- organization_list() %>% 
+      `[`(c("title", "name"))
+    colnames(retval) <- c("title", "course_name")
+    retval
+  } else {
+    retval <- organization_show(course_name)
+    packages <- retval$packages %>%
+      `[`(c("title"))
+    packages$url <- lapply(retval$packages$resource, `[[`, "url") %>%
+      sapply(`[`, 1)
+    packages <- packages[grepl("data.dsp.im", packages$url),]
+    packages$file_name <- strsplit(packages$url, "/") %>%
+      sapply(tail, 1)
+    if (is.null(getOption("DSPCKAN_DESTDIR")) || enter_destdir) input_destdir()
+    destdir <- getOption("DSPCKAN_DESTDIR")
+    if (is.null(destdir) || destdir == "") {
+      destdir <- normalizePath(tempdir())
+    }
+    for(i in seq_len(nrow(packages))) {
+      dest <- sprintf("%s/%s", destdir, packages$file_name[i]) %>%
+        normalizePath(mustWork = FALSE)
+      if (file.exists(dest)) next
+      cat(sprintf("\tDownloading %s...\n", packages$file_name[i]))
+      res <- httr::GET(packages$url[i], add_headers("X-CKAN-API-Key" = getOption("X-CKAN-API-Key", "")))
+      binary <- httr::content(res)
+      writeBin(binary, dest)
+    }
+    cat(sprintf("The data are downloaded to %s\n", destdir))
+  }
+}
+
 decode_hex_utf16 <- function(retval) {
-  for(i in seq_len(ncol(retval))) {
+  for(i in seq_along(retval)) {
     if (class(retval[[i]]) == "character") retval[[i]] <- .decode_hex_utf16(retval[[i]])
+    if (class(retval[[i]]) == "data.frame") retval[[i]] <- decode_hex_utf16(retval[[i]])
+    if (class(retval[[i]]) == "list") retval[[i]] <- decode_hex_utf16(retval[[i]])
   }
   retval
 }
@@ -49,10 +91,14 @@ decode_hex_utf16 <- function(retval) {
 
 #'@importFrom svGUI is.gui
 #'@importFrom svDialogs dlgForm
-#'@export
 input_apikey <- function() {
   Form <- list("API-Key:TXT" = "")
   options("X-CKAN-API-Key" = dlgForm(Form, "My API-Key")$res[["API-Key"]])
   NULL
 }
 
+input_destdir <- function() {
+  Form <- list("DESTDIR:TXT" = "")
+  options("DSPCKAN_DESTDIR" = dlgForm(Form, "Destination")$res[["DESTDIR"]])
+  NULL
+}
