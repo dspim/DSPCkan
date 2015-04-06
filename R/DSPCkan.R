67766e28af23034fb4106188485b2b321e59475a
Wush Wu <wush978@gmail.com>
2015-04-06 12:53:27 +0800

    implement organization decode all utf code print import httr::add_headers for authentication

--- R/DSPCkan.R
+++ R/DSPCkan.R
@@ -1,24 +1,37 @@
-.url <- "http://data.dsp.im/"
+.url <- "https://data.dsp.im"
 
-.apikey <- NULL
-
+#'@importFrom httr add_headers
 #'@export
-package_list <- function(as = "table") {
-  ckanr::package_list(as = as, url = .url)
+package_list <- function(offset = 0, limit = 31, as = "table") {
+  ckanr::package_list(offset = offset, limit = limit, as = as, url = .url, 
+                      add_headers("X-CKAN-API-Key" = getOption("X-CKAN-API-Key", ""))) %>%
+    decode_hex_utf16
 }
 
 #'@export
 tag_list <- function(as = "table") {
-  retval <- ckanr::tag_list(as = as, url = .url)
+  ckanr::tag_list(query = NULL, vocabulary_id = NULL, all_fields = TRUE,
+                  as = as, url = .url, add_headers("X-CKAN-API-Key" = getOption("X-CKAN-API-Key", ""))) %>%
+    decode_hex_utf16
+}
+
+#'@export
+organization_list <- function(id, as = "table") {
+  ckanr::organization_list(order_by = "name", decreasing = TRUE, organizations = NULL, all_fields = TRUE,
+                          as = as, url = .url, add_headers("X-CKAN-API-Key" = getOption("X-CKAN-API-Key", ""))) %>%
+    decode_hex_utf16
+}
+
+decode_hex_utf16 <- function(retval) {
   for(i in seq_len(ncol(retval))) {
-    if (class(retval[[i]]) == "character") retval[[i]] <- decode_hex_utf16(retval[[i]])
+    if (class(retval[[i]]) == "character") retval[[i]] <- .decode_hex_utf16(retval[[i]])
   }
   retval
 }
 
 #'@importFrom magrittr %>%
-decode_hex_utf16 <- function(x) {
-  m <- regmatches(x, regexec("(<U\\+[0-9a-f]{4}>)+", x))
+.decode_hex_utf16 <- function(x, m = NULL) {
+  if (is.null(m)) m <- regmatches(x, regexec("(<U\\+[0-9a-f]{4}>)+", x))
   retval <- x
   for(i in seq_along(m)) {
     if (length(m[[i]]) == 0) next
@@ -29,6 +42,8 @@ decode_hex_utf16 <- function(x) {
     }) %>% paste(collapse = "")
     retval[i] <- gsub(pattern = m[[i]][1], replacement = value, x = x[i], fixed = TRUE)
   }
+  m <- regmatches(retval, regexec("(<U\\+[0-9a-f]{4}>)+", retval))
+  if (!all(sapply(m, length) == 0)) retval <- .decode_hex_utf16(retval, m)
   retval
 }
 
@@ -37,6 +52,7 @@ decode_hex_utf16 <- function(x) {
 #'@export
 input_apikey <- function() {
   Form <- list("API-Key:TXT" = "")
-  dlgForm(Form, "My API-Key")$res
+  options("X-CKAN-API-Key" = dlgForm(Form, "My API-Key")$res[["API-Key"]])
+  NULL
 }
 
